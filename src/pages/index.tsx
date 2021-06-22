19888110623ff363877792e62f4cb57a7103db93
cmpswierczek <81811185+cmpswierczek@users.noreply.github.com>
2021-06-22 12:19:23 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,6 +97,7 @@ const Index: FunctionComponent<Props> = props => {
           <ImageFile />
         </Section>
       </div>
+      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/hXqv57GqMB4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div ref={refs.newsletter}>
         <Section id="newsletter">
           <Newsletter />
