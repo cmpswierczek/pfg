4b48f5807f360d9aaee230fae671fed04100ecec
cmpswierczek <81811185+cmpswierczek@users.noreply.github.com>
2023-01-23 11:03:24 +0100

    Create rodo.tsx

---
+++ src/pages/rodo.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+import Layout from "../components/Layout/Layout"
+import SEO from "../components/SEO"
+import Bariatry from "../components/Bariatry/Bariatry"
+
+interface Props {}
+
+const PrivacyPolicyPage = (props: Props) => {
+  return (
+    <Layout>
+      <SEO title="RODO" />
+      <section style={{padding: '4rem 7rem', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <h1 style={{marginBottom: '3rem'}}>RODO</h1>
+        <p>
+         Inspektorem Ochrony Danych Osobowych w:
+          1. Polskiej Fundacja Gastroenterologii z siedzibą w Warszawie (01-631), ul. Waryńskiego 10A,
+          2. Endoterapia Pfg Sp.z o. o. z siedzibą w Warszawie (01-631).
+             jest Pan Robert Wakoń, z którym można się kontaktować we wszystkich sprawach związanych z przetwarzaniem danych osobowych.
+        Możliwe formy kontaktu:
+        - listownie przesyłając korespondencję na nasz adres, z dopiskiem „IOD” 
+        - mailowo na adres: iod@pfg.org.pl.
+        </p>
+      </section>
+    </Layout>
+  )
+}
+
+export default PrivacyPolicyPage
